96f9d8ad0380a716d005acef25708269c0b92244
ljooss <bartkisius@gmail.com>
2019-02-15 17:54:27 +0200

    juda mikimauzzzaiii
    hyhyhyhyhyhyhyhyhyahyahyahayhayhyahyhyudgfysjuyferuretydurefygrerjet4reiyro3ot6y7uryt5eeytyt72ti7645t
    
    \
    
    \
    
    \\
    
    \\
    
    .
    
    .

---
+++ pickledpear/Assets/ENEMIES.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMIES : MonoBehaviour
+{
+    public float speed = 1.00f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed);
+
+        if (transform.position.x < -3f)
+        {
+            speed = speed * -1;
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.5f);
+        }
+
+
+        if (transform.position.x > 3f)
+        {
+            speed = speed * -1;
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.5f);
+
+        }
+    }
+}
